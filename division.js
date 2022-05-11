899766552e3333d03f13159ce01cc4c9ce80a055
melvinjim <melvinjimenezang@hotmail.com>
2022-05-11 13:35:02 -0500

    trying to perform two different tests

--- division.js
+++ division.js
@@ -2,6 +2,10 @@ class Division{
     firstResult(fristNumber, secondNumber){
         return fristNumber/secondNumber;
     }
+
+    secondtResult(fristNumber, secondNumber){
+        return fristNumber/secondNumber;
+    }
 }
 
 module.exports = Division;
--- main.spec.js
+++ main.spec.js
@@ -7,4 +7,10 @@ describe('resultados de divisiones', () => {
         const total = division.firstResult(6,2);
         expect(total).toBe(3);
     });
+
+    it('division entre los numeros 1 y 6 donde el resultado debe ser un numero periodico que es dismiinuido a solo dos numeros despues de la coma', () =>{
+        const total = division.secondtResult(1,6);
+        var seting = total.toFixed(2)
+        expect(seting).toBe(seting);
+    });
 });
