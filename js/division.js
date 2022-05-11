e1745d50545c3dc3f91d6cac44fb47aa5126478e
melvinjim <melvinjimenezang@hotmail.com>
2022-05-11 09:29:12 -0500

    corrections in the code

--- js/division.js
+++ js/division.js
@@ -1,10 +1,3 @@
-document.getElementById("division").addEventListener('click', function() {
-    var value1 = document.getElementById("value1").value;
-    var value2 = document.getElementById("value2").value;
-    var result = divison(value1, value2);
-    document.getElementById("result").innerHTML = "el total es: " + result;
-});
-
 function divison(firstValue, secodnValue) {
     total = firstValue/secodnValue;
     return total;
