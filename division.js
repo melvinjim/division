7b46e702c97a77ae31570c85053bd93cc8a202dd
melvinjim <melvinjimenezang@hotmail.com>
2022-05-11 14:47:17 -0500

    four different tests

--- division.js
+++ division.js
@@ -6,6 +6,18 @@ class Division{
     secondtResult(fristNumber, secondNumber){
         return fristNumber/secondNumber;
     }
+
+    thirdResult(fristNumber, secondNumber){
+        return fristNumber/secondNumber;
+    }
+
+    forthResult(fristNumber, secondNumber){
+        if (secondNumber < 1) {
+            return 'error, cant divide by 0'
+        }
+        return fristNumber/secondNumber;
+    }
 }
+
 
 module.exports = Division;
--- main.spec.js
+++ main.spec.js
@@ -1,16 +1,37 @@
-const { it, expect} = require("@jest/globals");
+const { it, expect } = require("@jest/globals");
 const Division = require("./division");
 
-describe('resultados de divisiones', () => {
+describe('first test result of divisions', () => {
     const division = new Division();
-    it('division entre los numeros 6 y 2 donde el resultado debe ser 3', () =>{
+    it('division between the numbers 6 and 2 where the result must be 3', () =>{
         const total = division.firstResult(6,2);
         expect(total).toBe(3);
     });
+});
 
-    it('division entre los numeros 1 y 6 donde el resultado debe ser un numero periodico que es dismiinuido a solo dos numeros despues de la coma', () =>{
+describe('second test result of divisions', () => {
+    const division = new Division();
+    it('division between the numbers 1 and 6 where the result must be a periodic number that is reduced to only two numbers after the comma', () =>{
         const total = division.secondtResult(1,6);
         var seting = total.toFixed(2)
         expect(seting).toBe(seting);
+    });
+});
+
+describe('third test result of divisions', () => {
+    const division = new Division();
+    it('division between a text type parameter and a numeric type parameter that should result in NaN', () =>{
+        const total = division.thirdResult("Melvin",6);
+        if (isNaN(total)) {
+            expect(total).toBe(NaN);
+        }
+    });
+});
+
+describe('fourth test result of divisions', () => {
+    const division = new Division();
+    it('division that cannot be performed and will result in an error message', () =>{
+        const total = division.forthResult(5,0);
+        expect(total).toBe("error, cant divide by 0");
     });
 });
